8637be8345c0ab6b42c6001f9fe39b20d7c1d51d
Matthew Ramina <mattramina@gmail.com>
2020-05-04 23:35:11 -0400

    Updates

---
+++ precache-manifest.022d09c7023b7287e901f1b1136e83e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43de42de000d9e21fc5dd30812d24a78",
+    "url": "/ru-course-history/index.html"
+  },
+  {
+    "revision": "20bb2dd2e8b674e254dc",
+    "url": "/ru-course-history/static/css/main.22c6eb43.chunk.css"
+  },
+  {
+    "revision": "8a253dd6b6dab61107a5",
+    "url": "/ru-course-history/static/js/2.7532813d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ru-course-history/static/js/2.7532813d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20bb2dd2e8b674e254dc",
+    "url": "/ru-course-history/static/js/main.1811a78b.chunk.js"
+  },
+  {
+    "revision": "9c8e9f4abd5ac2f17bf9",
+    "url": "/ru-course-history/static/js/runtime-main.ed789007.js"
+  }
+]);
